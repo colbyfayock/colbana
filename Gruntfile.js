0fa93bae7710deb3541d113a274c9a050a42c96a
Colby Fayock <fayock@gmail.com>
2016-10-23 23:29:38 -0400

    first commit of real site

---
+++ Gruntfile.js
@@ -0,0 +1,139 @@
+module.exports = function(grunt) {
+
+    require('jit-grunt')(grunt);
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+
+            options: {
+                separator: ';',
+            },
+
+            us: {
+                src: [
+
+                    // Bower components
+
+                    './bower_components/modernizr/modernizr.js',
+
+
+                    // Main
+
+                    './build/us/js/main.js'
+
+                ],
+                dest: './us/js/colbana.js',
+            },
+
+        },
+
+        uglify: {
+
+            options: {
+                banner: '/*\n' +
+                    ' * <%= pkg.name %>\n' +
+                    ' */\n',
+                mangle: false,
+                compress: false
+            },
+
+            us: {
+                files: {
+                    './us/js/colbana.min.js': [
+                        './us/js/colbana.js'
+                    ]
+                }
+            }
+
+        },
+
+        sass: {
+
+            options: {
+                sourceMap: true,
+                outputStyle: 'compressed'
+            },
+
+            us: {
+                files: {
+                    './us/css/colbana.min.css': './build/us/scss/main.scss'
+                }
+            }
+
+        },
+
+        rsync: {
+            options: {
+                args: [
+                    "-avhzS --progress"
+                ],
+                exclude: [
+
+                    ".git*",
+
+                    "build",
+
+                    "bower_components",
+                    "node_modules",
+
+                    "package.json",
+                    "bower.json",
+                    "*.sublime*",
+
+                ],
+                recursive: true
+            },
+            deploy: {
+                options: {
+                    src: '<%= env.rsync.deploy.src %>',
+                    dest: '<%= env.rsync.deploy.dest %>',
+                    host: '<%= env.rsync.deploy.host %>',
+                    delete: false
+                }
+            }
+        },
+
+        watch: {
+
+            us_css: {
+                files: './build/us/scss/**/*.scss',
+                tasks: [
+                    'us_css'
+                ]
+            },
+
+            us_js: {
+                files: './build/us/js/**/*.js',
+                tasks: [
+                    'us_js'
+                ]
+            },
+
+        }
+
+    });
+
+    grunt.registerTask('default', [
+    ]);
+
+    grunt.registerTask('us_css', [
+        'sass:us'
+    ]);
+
+    grunt.registerTask('css', [
+        'us_css'
+    ]);
+
+    grunt.registerTask('us_js', [
+        'concat:us',
+        'uglify:us'
+    ]);
+
+    grunt.registerTask('js', [
+        'us_js'
+    ]);
+
+};
